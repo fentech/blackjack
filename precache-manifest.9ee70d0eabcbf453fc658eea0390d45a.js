fc17e1e7c22905bbf60f008c86d1c992cf3ed134
Joe Fensler <joe.fensler@gmail.com>
2020-04-07 07:14:17 -0500

    Updates

---
+++ precache-manifest.9ee70d0eabcbf453fc658eea0390d45a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/blackjack/favicon-16.png"
+  },
+  {
+    "revision": "f89eb144af114210a1cd",
+    "url": "/blackjack/static/js/app.09d57ae5.chunk.js"
+  },
+  {
+    "revision": "02b41a81376aeaf19dc7",
+    "url": "/blackjack/static/js/runtime~app.3373d06e.js"
+  },
+  {
+    "revision": "2904bac1a7659b0635df",
+    "url": "/blackjack/static/js/2.5a33bd79.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/blackjack/serve.json"
+  },
+  {
+    "revision": "a58fa97eaf87c593bde0df923fa3946c",
+    "url": "/blackjack/register-service-worker.js"
+  },
+  {
+    "revision": "e3982a52fa9cf9c58c430340d418afa4",
+    "url": "/blackjack/manifest.json"
+  },
+  {
+    "revision": "2f61d76f13dc9db60467f5443f8319b5",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/blackjack/favicon.ico"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/blackjack/favicon-32.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/blackjack/expo-service-worker.js"
+  }
+];

c816c5d23aa03e770dc37c40b73d5f347c27b42b
Joe Fensler <joe.fensler@gmail.com>
2020-04-07 07:02:54 -0500

    build: add test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)",
   ],
+  coverageReporters: ["json-summary", "text", "lcov"],
 };

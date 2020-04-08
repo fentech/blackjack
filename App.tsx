b5a0527903039b15b5fc4361fd0da67f36b40d4f
Joe Fensler <joe.fensler@gmail.com>
2020-04-08 16:04:40 -0500

    feat: use custom theme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { ApplicationProvider } from "@ui-kitten/components";
-import { mapping, light as lightTheme } from "@eva-design/eva";
+import { mapping } from "@eva-design/eva";
+import { default as customMapping } from "./assets/custom-theme.json";
 import Game from "./app/features/Game/Game";
 import store from "./app/store";
 
 export default function App() {
   return (
-    <ApplicationProvider mapping={mapping} theme={lightTheme}>
+    <ApplicationProvider mapping={mapping} theme={customMapping}>
       <Provider store={store}>
         <Game />
       </Provider>

754556399e29d5cd64604aa1d33dceee5a5127f0
Joe Fensler <joe.fensler@gmail.com>
2020-04-24 11:12:42 -0500

    build: ignore deck in state check

--- app/store.ts
+++ app/store.ts
@@ -1,12 +1,23 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import game from "./features/Game/gameSlice";
 const rootReducer = combineReducers({
   game,
 });
 export type RootState = ReturnType<typeof rootReducer>;
 
+const defaultMiddleware = getDefaultMiddleware({
+  immutableCheck: {
+    ignoredPaths: ["game.deck"],
+  },
+});
+
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [...defaultMiddleware],
 });
 export type AppDispatch = typeof store.dispatch;
 
